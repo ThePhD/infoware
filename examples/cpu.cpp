1d5270036f379ca7ae634bb24baf1e460dc0570a
nabijaczleweli <nabijaczleweli@gmail.com>
2016-06-19 13:47:47 +0200

    Split tests into logical units
    Closes #4

---
+++ examples/cpu.cpp
@@ -0,0 +1,105 @@
+// infoware - C++ System information Library
+//
+// Written in 2016 by nabijaczleweli <nabijaczleweli@gmail.com> and ThePhD <phdofthehouse@gmail.com>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and related
+// and neighboring rights to this software to the public domain worldwide. This software is
+// distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along with this software.
+// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>
+
+
+#include "infoware/cpu.hpp"
+#include <iostream>
+#include <utility>
+
+
+static const char* cache_type_name(iware::cpu::cache_type_t cache_type) noexcept;
+static const char* architecture_name(iware::cpu::architecture_t architecture) noexcept;
+static const char* endianness_name(iware::cpu::endianness_t endianness) noexcept;
+
+
+int main() {
+	{
+		const auto quantities = iware::cpu::quantities();
+		std::cout << "\n"
+		             "  Quantities:\n"
+		          << "    Logical CPUs : " << quantities.logical << '\n'
+		          << "    Physical CPUs: " << quantities.physical << '\n'
+		          << "    CPU packages : " << quantities.packages << '\n';
+	}
+
+	{
+		std::cout << "\n"
+		             "  Caches:\n";
+		for(auto i = 1u; i <= 3; ++i) {
+			const auto cache = iware::cpu::cache(i);
+			std::cout << "    L" << i << ":\n"
+			          << "      Size         : " << cache.size << "B\n"
+			          << "      Line size    : " << cache.line_size << "B\n"
+			          << "      Associativity: " << static_cast<unsigned int>(cache.associativity) << '\n'
+			          << "      Type         : " << cache_type_name(cache.type) << '\n';
+		}
+	}
+
+	{
+		std::cout << "\n"
+		          << "  Architecture: " << architecture_name(iware::cpu::architecture()) << '\n'
+		          << "  Frequency: " << iware::cpu::frequency() << "Hz\n"
+		          << "  Endianness: " << endianness_name(iware::cpu::endianness()) << '\n'
+		          << "  Model name: " << iware::cpu::model_name() << '\n';
+	}
+
+	{
+		std::cout << std::boolalpha << "\n"
+		                               "  Instruction set support:\n";
+		for(auto&& set : {std::make_pair("3D-now!", iware::cpu::instruction_set_t::ThreeD_now),  //
+		                  std::make_pair("MMX    ", iware::cpu::instruction_set_t::MMX),         //
+		                  std::make_pair("SSE    ", iware::cpu::instruction_set_t::SSE),         //
+		                  std::make_pair("SSE2   ", iware::cpu::instruction_set_t::SSE2),        //
+		                  std::make_pair("SSE3   ", iware::cpu::instruction_set_t::SSE3),        //
+		                  std::make_pair("AVX    ", iware::cpu::instruction_set_t::AVX)})
+			std::cout << "    " << set.first << ": " << iware::cpu::instruction_set_supported(set.second) << '\n';
+	}
+
+	std::cout << '\n';
+}
+
+
+static const char* cache_type_name(iware::cpu::cache_type_t cache_type) noexcept {
+	switch(cache_type) {
+		case iware::cpu::cache_type_t::unified:
+			return "Unified";
+		case iware::cpu::cache_type_t::instruction:
+			return "Instruction";
+		case iware::cpu::cache_type_t::data:
+			return "Data";
+		case iware::cpu::cache_type_t::trace:
+			return "Trace";
+	}
+}
+
+static const char* architecture_name(iware::cpu::architecture_t architecture) noexcept {
+	switch(architecture) {
+		case iware::cpu::architecture_t::x64:
+			return "x64";
+		case iware::cpu::architecture_t::ARM:
+			return "ARM";
+		case iware::cpu::architecture_t::itanium:
+			return "Itanium";
+		case iware::cpu::architecture_t::x86:
+			return "x86";
+		case iware::cpu::architecture_t::unknown:
+			return "Unknown";
+	}
+}
+
+static const char* endianness_name(iware::cpu::endianness_t endianness) noexcept {
+	switch(endianness) {
+		case iware::cpu::endianness_t::little:
+			return "Little-Endian";
+		case iware::cpu::endianness_t::big:
+			return "Big-Endian";
+	}
+}
---
+++ examples/system.cpp
@@ -0,0 +1,86 @@
+// infoware - C++ System information Library
+//
+// Written in 2016 by nabijaczleweli <nabijaczleweli@gmail.com> and ThePhD <phdofthehouse@gmail.com>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and related
+// and neighboring rights to this software to the public domain worldwide. This software is
+// distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along with this software.
+// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>
+
+
+#include "infoware/system.hpp"
+#include <iostream>
+
+
+static const char* kernel_variant_name(iware::system::kernel_t variant) noexcept;
+
+
+int main() {
+	{
+		std::cout << "\n"
+		             "  Connected HIDs:\n"
+		          << "    Mice     : " << iware::system::mouse_amount() << '\n'
+		          << "    Keyboards: " << iware::system::keyboard_amount() << '\n'
+		          << "    Other    : " << iware::system::other_HID_amount() << '\n';
+	}
+
+	{
+		const auto memory = iware::system::memory();
+		std::cout << "\n"
+		             "  Memory:\n"
+		             "    Physical:\n"
+		          << "      Available: " << memory.physical_available << "B\n"
+		          << "      Total    : " << memory.physical_total << "B\n"
+		          << "    Virtual:\n"
+		          << "      Available: " << memory.virtual_available << "B\n"
+		          << "      Total    : " << memory.virtual_total << "B\n";
+	}
+
+	{
+		const auto kernel_info = iware::system::kernel_info();
+		std::cout << "\n"
+		             "  Kernel:\n"
+		          << "    Variant: " << kernel_variant_name(kernel_info.variant) << '\n'
+		          << "    Version: " << kernel_info.major << '.' << kernel_info.minor << '.' << kernel_info.patch << " build " << kernel_info.build_number << '\n';
+	}
+
+	{
+		const auto OS_info = iware::system::OS_info();
+		std::cout << "\n"
+		             "  OS:\n"
+		          << "    Name     : " << OS_info.name << '\n'
+		          << "    Full name: " << OS_info.full_name << '\n'
+		          << "    Version  : " << OS_info.major << '.' << OS_info.minor << '.' << OS_info.patch << " build " << OS_info.build_number << '\n';
+	}
+
+	{
+		const auto displays = iware::system::displays();
+		std::cout << "\n"
+		             "  Displays:\n";
+		for(auto i = 0u; i < displays.size(); ++i) {
+			const auto& display = displays[i];
+			std::cout << "    #" << (i + 1) << ":\n"
+			          << "      Resolution  : " << display.width << 'x' << display.height << '\n'
+			          << "      DPI         : " << display.dpi << '\n'
+			          << "      Colour depth: " << display.bpp << "b\n";
+		}
+	}
+
+	std::cout << '\n';
+}
+
+
+static const char* kernel_variant_name(iware::system::kernel_t variant) noexcept {
+	switch(variant) {
+		case iware::system::kernel_t::windows_nt:
+			return "Windows NT";
+		case iware::system::kernel_t::linux:
+			return "Linux";
+		case iware::system::kernel_t::darwin:
+			return "Darwin";
+		case iware::system::kernel_t::unknown:
+			return "Unknown";
+	}
+}

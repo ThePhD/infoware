0e1a0c9e5f6d6abbb04430633f1567cc96e8cf8b
nabijaczleweli <nabijaczleweli@gmail.com>
2019-10-29 07:17:23 +0100

    Parse /etc/os-release before trying /etc/lsb-release
    Ref: #31

--- src/system/OS_info/os_info_non_windows_non_darwin.cpp
+++ src/system/OS_info/os_info_non_windows_non_darwin.cpp
@@ -17,15 +17,9 @@
 #include "infoware/system.hpp"
 #include <cstdlib>
 #include <fstream>
-#include <sys/utsname.h>
 
 
-iware::system::OS_info_t iware::system::OS_info() {
-	std::ifstream release("/etc/lsb-release");
-
-	if(!release.is_open() || !release)
-		return {};
-
+static iware::system::OS_info_t lsb_release(std::ifstream& release) {
 	iware::system::OS_info_t ret{};
 
 	for(std::string line; std::getline(release, line);)
@@ -42,6 +36,59 @@ iware::system::OS_info_t iware::system::OS_info() {
 			const auto end_idx   = line.size() - 1;
 			ret.full_name        = line.substr(start_idx, end_idx - start_idx);
 		}
+
+	return ret;
+}
+
+static void trim_quotes(std::string& in) {
+	if(in.empty())
+		return;
+
+	if(in.back() == '"')
+		in.pop_back();
+
+	if(in.front() == '"')
+		in.erase(in.begin());
+}
+
+
+// https://www.linux.org/docs/man5/os-release.html
+iware::system::OS_info_t iware::system::OS_info() {
+	std::ifstream release("/etc/os-release");
+
+	if(!release.is_open() || !release) {
+		release.open("/usr/lib/os-release", std::ios::in);
+		if(!release.is_open() || !release) {
+			release.open("/etc/lsb-release", std::ios::in);
+			if(!release.is_open() || !release)
+				return {};
+			else
+				return lsb_release(release);
+		}
+	}
+
+	iware::system::OS_info_t ret{};
+
+	for(std::string line; std::getline(release, line);)
+		if(line.find("NAME") == 0)
+			ret.name = line.substr(line.find('=') + 1);
+		else if(line.find("PRETTY_NAME") == 0)
+			ret.full_name = line.substr(line.find('=') + 1);
+		else if(line.find("VERSION_ID") == 0) {
+			char* marker = &line[line.find('=') + 1];
+			if(marker[0] == '"')
+				++marker;
+			ret.major = std::strtoul(marker, &marker, 10);
+			if(marker[0] && marker[0] != '"')
+				ret.minor = std::strtoul(marker + 1, &marker, 10);
+			if(marker[0] && marker[0] != '"')
+				ret.patch = std::strtoul(marker + 1, &marker, 10);
+			if(marker[0] && marker[0] != '"')
+				ret.build_number = std::strtoul(marker + 1, nullptr, 10);
+		}
+
+	trim_quotes(ret.name);
+	trim_quotes(ret.full_name);
 
 	return ret;
 }

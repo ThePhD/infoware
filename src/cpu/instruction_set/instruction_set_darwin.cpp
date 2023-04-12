8b8c875e87cb9e3844c8f66e06aebae1271c5cf9
Frost <91168148+Frost-54@users.noreply.github.com>
2023-04-13 00:00:39 +1000

    Add global mappings for arm

--- src/cpu/instruction_set/instruction_set_darwin.cpp
+++ src/cpu/instruction_set/instruction_set_darwin.cpp
@@ -21,12 +21,51 @@
 
 
 static std::pair<const char* const*, const char* const*> instruction_set_to_names(iware::cpu::instruction_set_t set) {
-	static const char* mmx[]       = {"MMX"};
-	static const char* threednow[] = {"3DNOW", "3DNOWEXT"};
-	static const char* sse[]       = {"SSE"};
-	static const char* sse2[]      = {"SSE2"};
-	static const char* sse3[]      = {"SSSE3", "SSE3"};
-	static const char* avx[]       = {"AVX"};
+	static const char* mmx[]               = {"MMX"};
+	static const char* threednow[]         = {"3DNOW", "3DNOWEXT"};
+	static const char* sse[]               = {"SSE"};
+	static const char* sse2[]              = {"SSE2"};
+	static const char* sse3[]              = {"SSSE3", "SSE3"};
+	static const char* avx[]               = {"AVX"};
+      static const char* fhm[]               = { "FHM" };
+      static const char* dotprod[]           = { "DOTPROD" };                  
+      static const char* rdm[]               = { "RDM" };                            
+      static const char* lse[]               = { "LSE" };                            
+      static const char* pmnull[]            = { "PMNULL" };                      
+      static const char* specres[]           = { "SPECRES" };                    
+      static const char* sb[]                = { "SB" };                              
+      static const char* frintts[]           = { "FRINTTS" };                    
+      static const char* lrcpc[]             = { "LRCPC" };                        
+      static const char* lrcpc2[]            = { "LRCPC2" };                      
+      static const char* fcma[]              = { "FCMA" };                          
+      static const char* jscvt[]             = { "JSCVT" };                        
+      static const char* pauth[]             = { "PAUTH" };                        
+      static const char* pauth2[]            = { "PAUTH2" };                      
+      static const char* fpac[]              = { "FPAC" };                          
+      static const char* dpb[]               = { "DPB" };                            
+      static const char* dpb2[]              = { "DPB2" };                          
+      static const char* bf16[]              = { "BF16" };                          
+      static const char* i8mm[]              = { "I8MM" };                          
+      static const char* ecv[]               = { "ECV" };                            
+      static const char* les2[]              = { "LES2" };                          
+      static const char* csv2[]              = { "CSV2" };                          
+      static const char* csv3[]              = { "CSV3" };                          
+      static const char* dit[]               = { "DIT" };                            
+      static const char* fp16[]              = { "FP16" };                          
+      static const char* ssbs[]              = { "SSBS" };                          
+      static const char* bti[]               = { "BTI" };                            
+      static const char* fp_sync_exception[] = { "FP_SYNC_EXCEPTION" };
+      static const char* neon[]              = { "NEON" };                          
+      static const char* armv8_1_atomics[]   = { "ARMV8_1_ATOMICS" };    
+      static const char* armv8_2_fhm[]       = { "ARMV8_2_FHM" };            
+      static const char* armv8_2_compnum[]   = { "ARMV8_2_COMPNUM" };    
+      static const char* watchpoint[]        = { "WATCHPOINT" };              
+      static const char* breakpoint[]        = { "BREAKPOINT" };              
+      static const char* armv8_crc32[]       = { "ARMV8_CRC32" };            
+      static const char* armv8_gpi[]         = { "ARMV8_GPI" };                
+      static const char* adv_simd[]          = { "ADV_SIMD" };                  
+      static const char* adv_simd_hpfp_cvt[] = { "ADV_SIMD_HPFP_CVT" };
+      static const char* ucnormal_mem[]      = { "UCNORMAL_MEM" };          
 
 
 #define HANDLED_CASE(iset, variable)        \
@@ -40,6 +79,45 @@ static std::pair<const char* const*, const char* const*> instruction_set_to_name
 		HANDLED_CASE(sse2, sse2)
 		HANDLED_CASE(sse3, sse3)
 		HANDLED_CASE(avx, avx)
+            HANDLED_CASE(fhm, fhm)
+            HANDLED_CASE(dotprod, dotprod)
+            HANDLED_CASE(rdm, rdm)
+            HANDLED_CASE(lse, lse)
+            HANDLED_CASE(pmnull, pmnull)
+            HANDLED_CASE(specres, specres)
+            HANDLED_CASE(sb, sb)
+            HANDLED_CASE(frintts, frintts)
+            HANDLED_CASE(lrcpc, lrcpc)
+            HANDLED_CASE(lrcpc2, lrcpc2)
+            HANDLED_CASE(fcma, fcma)
+            HANDLED_CASE(jscvt, jscvt)
+            HANDLED_CASE(pauth, pauth)
+            HANDLED_CASE(pauth2, pauth2)
+            HANDLED_CASE(fpac, fpac)
+            HANDLED_CASE(dpb, dpb)
+            HANDLED_CASE(dpb2, dpb2)
+            HANDLED_CASE(bf16, bf16)
+            HANDLED_CASE(i8mm, i8mm)
+            HANDLED_CASE(ecv, ecv)
+            HANDLED_CASE(les2, les2)
+            HANDLED_CASE(csv2, csv2)
+            HANDLED_CASE(csv3, csv3)
+            HANDLED_CASE(dit, dit)
+            HANDLED_CASE(fp16, fp16)
+            HANDLED_CASE(ssbs, ssbs)
+            HANDLED_CASE(bti, bti)
+            HANDLED_CASE(fp_sync_exception, fp_sync_exception)
+            HANDLED_CASE(neon, neon)
+            HANDLED_CASE(armv8_1_atomics, armv8_1_atomics)
+            HANDLED_CASE(armv8_2_fhm, armv8_2_fhm)
+            HANDLED_CASE(armv8_2_compnum, armv8_2_compnum)
+            HANDLED_CASE(watchpoint, watchpoint)
+            HANDLED_CASE(breakpoint, breakpoint)
+            HANDLED_CASE(armv8_crc32, armv8_crc32)
+            HANDLED_CASE(armv8_gpi, armv8_gpi)
+            HANDLED_CASE(adv_simd, adv_simd)
+            HANDLED_CASE(adv_simd_hpfp_cvt, adv_simd_hpfp_cvt)
+            HANDLED_CASE(ucnormal_mem, ucnormal_mem)
 
 		default:
 			return {nullptr, nullptr};
